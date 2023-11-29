af4bc0ca3f5a9d14268b5500b7c4a222dd4b036a
WignerSantos <wigner12@gmail.com>
2023-11-29 13:57:41 -0300

    Adding create method for Chamado

--- src/main/java/com/wigner/helpdesk/Services/ChamadoService.java
+++ src/main/java/com/wigner/helpdesk/Services/ChamadoService.java
@@ -2,8 +2,14 @@ package com.wigner.helpdesk.Services;
 
 import com.wigner.helpdesk.Services.exceptions.ObjectNotFoundException;
 import com.wigner.helpdesk.domain.Chamado;
+import com.wigner.helpdesk.domain.Cliente;
+import com.wigner.helpdesk.domain.Tecnico;
+import com.wigner.helpdesk.domain.dtos.ChamadoDto;
+import com.wigner.helpdesk.domain.enums.Prioridade;
+import com.wigner.helpdesk.domain.enums.Status;
 import com.wigner.helpdesk.repositories.ChamadoRepository;
 import com.wigner.helpdesk.resources.ChamadoResources;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +22,12 @@ public class ChamadoService {
     @Autowired
     private ChamadoRepository chamadoRepository;
 
+    @Autowired
+    private TecnicoService tecnicoService;
+
+    @Autowired
+    private ClienteService clienteService;
+
     public Chamado findById(Integer id) {
         Optional<Chamado> obj = chamadoRepository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
@@ -23,6 +35,28 @@ public class ChamadoService {
 
     public List<Chamado> findAll() {
         return chamadoRepository.findAll();
+    }
+
+    public Chamado create(@Valid ChamadoDto chamadoDto) {
+        return chamadoRepository.save(newChamado(chamadoDto));
+    }
+
+    private Chamado newChamado(ChamadoDto chamadoDto) {
+        Tecnico tecnico = tecnicoService.findById(chamadoDto.getTecnico());
+        Cliente cliente = clienteService.findById(chamadoDto.getCliente());
+
+        Chamado chamado = new Chamado();
+        if(chamadoDto.getId() != null) {
+            chamado.setId(chamadoDto.getId());
+        }
+
+        chamado.setTecnico(tecnico);
+        chamado.setCliente(cliente);
+        chamado.setPrioridade(Prioridade.toEnum(chamadoDto.getPrioridade()));
+        chamado.setStatus(Status.toEnum(chamadoDto.getStatus()));
+        chamado.setTitulo(chamadoDto.getTitulo());
+        chamado.setObservacoes(chamadoDto.getObservacoes());
+        return chamado;
     }
 
 }
--- src/main/java/com/wigner/helpdesk/resources/ChamadoResources.java
+++ src/main/java/com/wigner/helpdesk/resources/ChamadoResources.java
@@ -3,13 +3,13 @@ package com.wigner.helpdesk.resources;
 import com.wigner.helpdesk.Services.ChamadoService;
 import com.wigner.helpdesk.domain.Chamado;
 import com.wigner.helpdesk.domain.dtos.ChamadoDto;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,6 +31,14 @@ public class ChamadoResources {
         List<Chamado> chamados = chamadoService.findAll();
         List<ChamadoDto> chamadosDto = chamados.stream().map(ChamadoDto::new).toList();
         return ResponseEntity.ok().body(chamadosDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<ChamadoDto> create(@Valid @RequestBody ChamadoDto objDto) {
+        Chamado chamado = chamadoService.create(objDto);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(objDto.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
