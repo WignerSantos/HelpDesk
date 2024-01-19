e9c1b3fc3b04ac4c095f31eb9d021b365fe0c329
WignerSantos <wigner12@gmail.com>
2024-01-19 16:19:15 -0300

    Adding deploy support

--- src/main/java/com/wigner/helpdesk/config/SecurityConfig.java
+++ src/main/java/com/wigner/helpdesk/config/SecurityConfig.java
@@ -39,6 +39,23 @@ public class SecurityConfig {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
+//    @Bean
+//    public WebMvcConfigurer corsConfig() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("http://localhost:4200/**")
+//                        .allowedMethods(HttpMethod.GET.name(),
+//                                        HttpMethod.POST.name(),
+//                                        HttpMethod.PUT.name(),
+//                                        HttpMethod.DELETE.name(),
+//                                        HttpMethod.OPTIONS.name())
+//                        .allowedHeaders(HttpHeaders.CONTENT_TYPE, HttpHeaders.AUTHORIZATION);
+//            }
+//        };
+//    }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
